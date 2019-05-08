006d9fec3ec8dd481494130e649cb151aa1f1a37
Molly <qc8659ba@go.minneapolis.edu>
2019-05-08 17:51:32 -0500

    App is coded to the full extent of my abilities at this point. I am currently trying to connect all the pieces and upload it to heroku.
    Currently experiencing an error with curl. It's saying that javascript is not enabled in my project.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TasksForm v-on:task-added="newTaskAdded"></TasksForm>
+    <Playlist v-bind:tasks="tasks"></Playlist>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import TasksForm from './components/TasksForm.vue'
+  import Playlist from './components/Playlist'
+  export default {
+    name: 'app',
+    components: {
+      TasksForm,
+      Playlist
+    },
+    data(){
+      return{
+        tasks:[]
+      }
+    },
+    methods:{
+      newTaskAdded(task){
+        this.tasks.push(task);
+        var tisks = this.tasks;
+        for (var i = 0; i < tisks.length; i++) {
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+          // console.log(this.tasks[i])
+        }
+
+        // add task to list of tasks
+        //TODO: Add a sorting function to verify that tasks aren't happening concurrently
+        // sort them according to when they start
+        if(this.tasks.length>1)
+        //TODO: Make two sort functions, one for seeing if one task starts before the other, and one for validating that one task ends before the other begins.
+
+          this.tasks.sort(function (t1, t2) {
+            let thing = t1.stert-t2.stert;
+            if(thing<0){
+              let otherThing = t1.end - t2.stert;
+              if (otherThing>=0){
+                //TODO: show user error message
+                //TODO: how to prevent the entry from existing
+                console.log("One task must end before the other begins")
+              }
+            }else if (thing===0){
+              console.log("No multitasking allowed")
+            } else {
+              let otherThing = t2.end-t2.stert;
+              if(otherThing>=0){
+                console.log("One task must end before the other begins")
+              }
+            }
+            return thing
+          });
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
